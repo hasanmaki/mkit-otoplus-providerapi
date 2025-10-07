af7836b667f371f67f03a1e45563529415bbbbdf
Kafka Hasan Maki <182825954+hasanmaki@users.noreply.github.com>
2025-10-07 09:54:37 +0700

    experimenting response parsing

--- src/api/v1/dev.py
+++ src/api/v1/dev.py
@@ -1,40 +1,63 @@
-# src/api/v1/dev.py
 from fastapi import APIRouter
-from loguru import logger
+from fastapi.responses import Response
 
-from deps.digipos import DepDigiposApiClient, DepDigiposHttpService, DepDigiposSettings
-from src.core.utils.client_utils import handle_response, request_handler
+from core.client.base_model import NormalizedResponse
+from deps.digipos import DepDigiposHttpService, DepDigiposSettings
 
 router = APIRouter(prefix="/test", tags=["test"])
 
 
-@router.get(
-    "/test_raw",
-    summary="testing with raw cleint and service builder.",
-)
-async def explore_api(
-    client: DepDigiposApiClient,
-):
-    """Testing with raw cleint and service builder."""
-    endpoint = "command"
-    params = {"username": "WIR6289504"}
-    logger.debug(f"sending request to {endpoint} with params {params}")
+class PlainDictResponse(Response):
+    media_type = "text/plain"
 
-    try:
-        raw_response = await request_handler(client, "GET", endpoint, params=params)
-        result = handle_response(raw_response)
-    except Exception:
-        return {"message": "the app cant things again what kind of response its"}
-    return result
+    def render(self, content: NormalizedResponse | dict) -> bytes:
+        if isinstance(content, NormalizedResponse):
+            status = content.status_code
+            url = content.url
+            meta = str(content.meta).replace("'", "")
+            message = (
+                content.message
+                if hasattr(content.message, "value")
+                else str(content.message)
+            )
+            data = str(content.data).replace("'", "")
+            text = f"{status}&{url}&{meta}&{message}&{data}"
+        elif isinstance(content, dict):
+            text = str(content).replace("'", "")
+        else:
+            text = str(content)
+        return text.encode("utf-8")
 
 
-@router.get("/test_service")
+@router.get("/test_service", response_class=PlainDictResponse)
 async def test(service: DepDigiposHttpService, dgsettings: DepDigiposSettings):
     """Testing with injected service."""
     params = {"username": dgsettings.username}
     debug: bool = dgsettings.debug
-    endpoint: str = "command"
+    endpoint: str = "balance"
     result = await service.safe_request(
         "GET", endpoint=endpoint, params=params, debugresponse=debug
     )
-    return result
+    return PlainDictResponse(result)
+
+
+@router.get("/test_transaksi", response_class=PlainDictResponse)
+async def test_transaction(
+    service: DepDigiposHttpService, dgsettings: DepDigiposSettings
+):
+    """Testing with injected service."""
+    params = {
+        "username": dgsettings.username,
+        "to": "081295221639",
+        "category": "DATA",
+        "payment_method": "LINKAJA",
+        "json": 1,
+        "kolom": "productId,productName,quota,total_",
+        "up_harga": 100,
+    }
+    debug: bool = dgsettings.debug
+    endpoint: str = "list_paket"
+    result = await service.safe_request(
+        "GET", endpoint=endpoint, params=params, debugresponse=debug
+    )
+    return PlainDictResponse(result)
--- src/api/v1/digipos.py
+++ src/api/v1/digipos.py
@@ -1,72 +1,97 @@
-# """Router testing for development / exploration."""
+from typing import Annotated
 
-# from typing import Annotated
+from fastapi import APIRouter, Depends
 
-# from fastapi import APIRouter, Depends
-# from fastapi.responses import PlainTextResponse
+from deps.digipos import DepDigiposHttpService, DepDigiposSettings
+from schemas.sch_digipos import DGReqUsername, DGReqUsnOtp, DGReqUsnPass
 
-# from src.deps import ServiceDigiposAccount, get_digipos_account_service
-# from src.schemas.sch_digipos import DGReqUsername
-
-# router = APIRouter(prefix="/digipos", tags=["digipos"])
+router = APIRouter(prefix="/digipos", tags=["digipos"])
 
 
-# @router.get(
-#     "/login",
-#     summary="Forward login command to Digipos API",
-# )
-# async def login(query: Annotated[DGReqUsername, Depends()]):
-#     query
-#     return response_model
+@router.get(
+    path="/login",
+    summary="Forward login command to Digipos API",
+)
+async def login(
+    query: Annotated[DGReqUsnPass, Depends()],
+    http_service: DepDigiposHttpService,
+    settings: DepDigiposSettings,
+):
+    """Get login ke digipos Account API."""
+    params = {"username": query.username}
+    if query.username != settings.username:
+        raise ValueError("Username tidak sesuai dengan yang terdaftar")
+    if query.password != settings.password:
+        raise ValueError("Password tidak sesuai dengan yang terdaftar")
+    endpoint = settings.endpoints.login
+    debugresponse = settings.debug
+    response_model = await http_service.safe_request(
+        "GET", endpoint=endpoint, params=params, debugresponse=debugresponse
+    )
+    return response_model
 
 
-# @router.get(
-#     "/verify_otp",
-#     summary="Forward verify OTP command to Digipos API",
-# )
-# async def verify_otp(
-#     service: ServiceDigiposAccount = Depends(get_digipos_account_service), otp: str = ""
-# ):
-#     """Verify OTP untuk login ulang ke Digipos API."""
-#     response_model = await service.verify_otp(otp)
-#     return response_model
+@router.get(
+    "/verify_otp",
+    summary="Forward verify OTP command to Digipos API",
+)
+async def verify_otp(
+    query: Annotated[DGReqUsnOtp, Depends()],
+    http_service: DepDigiposHttpService,
+    settings: DepDigiposSettings,
+):
+    """Get verify OTP ke digipos Account API."""
+    params = {"username": query.username}
+    if query.username != settings.username:
+        raise ValueError("Username tidak sesuai dengan yang terdaftar")
+    endpoint = settings.endpoints.verify_otp
+    debugresponse = settings.debug
+    response_model = await http_service.safe_request(
+        "GET", endpoint=endpoint, params=params, debugresponse=debugresponse
+    )
+    return response_model
 
 
-# @router.get(
-#     path="/balance",
-#     summary="Forward balance command to Digipos API",
-#     responses={
-#         200: {
-#             "content": {
-#                 "text/plain": {
-#                     "example": "api_status_code=200#meta={'x-powered-by': 'Express', 'content_type': 'application/json; charset=utf-8', 'content_length': '146', 'elapsed_ms': 887.54, 'host': '10.0.0.3', 'path': '/balance', 'method': 'GET'}#data={'ngrs': {'1000': '0', '5000': '0', '10000': '0', '15000': '0', '20000': '0', '25000': '0', '50000': '0', '100000': '0', 'BULK': '0'}, 'linkaja': '3230', 'finpay': '0'}"
-#                 }
-#             }
-#         }
-#     },
-#     response_class=PlainTextResponse,
-# )
-# async def balance(
-#     service: ServiceDigiposAccount = Depends(get_digipos_account_service),
-# ):
-#     """Ambil balance dari Digipos API dan return sebagai text-friendly.
-#     Format:
-#     api_status_code=&meta=&data=
-#     """
-#     response_model = await service.get_balance()
-#     return response_model
+@router.get(
+    "/balance",
+    summary="Forward Balance command to Digipos API",
+)
+async def balance(
+    query: Annotated[DGReqUsername, Depends()],
+    http_service: DepDigiposHttpService,
+    settings: DepDigiposSettings,
+):
+    """Get verify OTP ke digipos Account API."""
+    params = {"username": query.username}
+    if query.username != settings.username:
+        raise ValueError("Username tidak sesuai dengan yang terdaftar")
+    endpoint = settings.endpoints.balance
+    debugresponse = settings.debug
+    response_model = await http_service.safe_request(
+        "GET", endpoint=endpoint, params=params, debugresponse=debugresponse
+    )
+    return response_model
 
 
-# @router.get(
-#     path="/profile",
-#     summary="Forward profile command to Digipos API",
-# )
-# async def profile(
-#     service: ServiceDigiposAccount = Depends(get_digipos_account_service),
-# ):
-#     """Ambil profile dari Digipos API."""
-#     response_model = await service.get_profile()
-#     return response_model
+@router.get(
+    path="/profile",
+    summary="Forward profile command to Digipos API",
+)
+async def profile(
+    query: Annotated[DGReqUsername, Depends()],
+    http_service: DepDigiposHttpService,
+    settings: DepDigiposSettings,
+):
+    """Ambil profile dari Digipos API."""
+    params = {"username": query.username}
+    if query.username != settings.username:
+        raise ValueError("Username tidak sesuai dengan yang terdaftar")
+    endpoint = settings.endpoints.profile
+    debugresponse = settings.debug
+    response_model = await http_service.safe_request(
+        "GET", endpoint=endpoint, params=params, debugresponse=debugresponse
+    )
+    return response_model
 
 
 # @router.get(
@@ -116,4 +141,29 @@
 # async def logout(service: ServiceDigiposAccount = Depends(get_digipos_account_service)):
 #     """Logout dari Digipos API."""
 #     response_model = await service.logout()
+#     return response_model
+
+
+# @router.get(
+#     path="/balance",
+#     summary="Forward balance command to Digipos API",
+#     responses={
+#         200: {
+#             "content": {
+#                 "text/plain": {
+#                     "example": "api_status_code=200#meta={'x-powered-by': 'Express', 'content_type': 'application/json; charset=utf-8', 'content_length': '146', 'elapsed_ms': 887.54, 'host': '10.0.0.3', 'path': '/balance', 'method': 'GET'}#data={'ngrs': {'1000': '0', '5000': '0', '10000': '0', '15000': '0', '20000': '0', '25000': '0', '50000': '0', '100000': '0', 'BULK': '0'}, 'linkaja': '3230', 'finpay': '0'}"
+#                 }
+#             }
+#         }
+#     },
+#     response_class=PlainTextResponse,
+# )
+# async def balance(
+#     service: ServiceDigiposAccount = Depends(get_digipos_account_service),
+# ):
+#     """Ambil balance dari Digipos API dan return sebagai text-friendly.
+#     Format:
+#     api_status_code=&meta=&data=
+#     """
+#     response_model = await service.get_balance()
 #     return response_model
